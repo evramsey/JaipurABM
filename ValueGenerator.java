367e7ac67f472a96b9c402a7ef6fc04fc3a5a590
evramsey <evramsey@ncsu.edu>
2020-05-16 14:40:48 -0400

    Update ValueGenerator.java

--- ValueGenerator.java
+++ ValueGenerator.java
@@ -1,51 +1,38 @@
-package JaipurABM;
-
 import ec.util.MersenneTwisterFast;
 import sim.util.distribution.*;
 
+/**
+ * Generate random samples pulled from a given distribution
+ */
 public class ValueGenerator {
 	static MersenneTwisterFast rng = new MersenneTwisterFast();
 
-	public static int getValueLikert(double avg, double stdDev) {
-		double normDistSample = createNormalDistSample(avg, stdDev);
-		while (normDistSample < 0 || normDistSample == 0) {
-			normDistSample = createNormalDistSample(avg, stdDev);
-		}
-		int intValue = (int) (Math.round(normDistSample));
-		return intValue;
-	}
-
+    /**
+     * Generate random sample from a normal distribution
+     * @param avg mean of the distribution
+     * @param stdDev standard deviation of the distribution
+     * @return sample value
+     */
 	public static double createNormalDistSample(double avg, double stdDev) {
-		double value = rng.nextGaussian();
-		double normDistSample = value * stdDev + avg;
+		double normDistSample = -1.0;
+		while(normDistSample < 0) {
+			double value = rng.nextGaussian();
+			normDistSample = value * stdDev + avg;
+		}
 		return normDistSample;
 	}
 
-
+    /**
+     * Generate random sample from a poisson distribution
+     * @param avg mean of the distribution
+     * @return sample value
+     */
 	public static int getPoissonValue(double avg) {
 		Poisson poissonDist = new Poisson(avg, rng);
 		int value = poissonDist.nextInt();
-		while (value == 0 || value < 0) {
+		while (value < 0) {
 			value = poissonDist.nextInt();
 		}
 		return value;
 	}
-
-	public static int getPoissonValueWithMaxAndMin(double avg, int min, int max) {
-		int value = getPoissonValue(avg);
-		while (value > max || value < min) {
-			value = getPoissonValue(avg);
-		}
-		return value;
-	}
-
-	public static double generateSocialPressureDelta(double avg, double stdDev) {
-		double normDistSample = createNormalDistSample(avg, stdDev);
-		while (normDistSample < 0 || normDistSample == 0) {
-			normDistSample = createNormalDistSample(avg, stdDev);
-		}
-		return normDistSample;
-	}
 }
-
-
